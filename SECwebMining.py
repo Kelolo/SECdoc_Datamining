fbba0abe69fd185c6e8689e64a4e60ce9fb43849
Allen Zhao <cz264@njit.edu>
2017-02-09 13:54:34 -0500

    added func for parsing all xml format txt file

--- SECwebMining.py
+++ SECwebMining.py
@@ -1,12 +1,14 @@
 import requests
 import urllib
+import xml.etree.cElementTree as et
+from tabulate import tabulate
 # from xml.dom import minidom
 import re
 try:
     from BeautifulSoup import BeautifulSoup
 except ImportError:
     from bs4 import BeautifulSoup
-
+# https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001166559&type=13F&dateb=&owner=exclude&count=100
 
 def constructURLfromCIKandDocType(CIK, DocType):
     urlPre = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK='
@@ -44,22 +46,91 @@ def findAllRequiredFileType(parsedHTML, requiredFileType):
 
 def parseXMLFromSoupObj(fileURL):
     parsedFile = inputURLgetParsedHTML(fileURL)
+    # stringParsedFile = str(parsedFile)
     if str(parsedFile)[0] == '<':
         parseMethod1_AllXML(parsedFile)
-        print(parsedFile)
+        # print(stringParsedFile)
         return
 
     parseMethod2_Ready(parsedFile)
-    print(parsedFile)
+
+    # print(parsedFile)
     return
 
+def cutAllXMLOut(string, start, end):
+    resXMLs = []
+    allStarts = [m.end() for m in re.finditer(start, string)]
+    allEnds = [m.start() for m in re.finditer(end, string)]
+    print(allStarts)
 
 def parseMethod1_AllXML(parsedFile):
-    print("All_XML")
+    print("**************** All_XML **********************")
+    # print(len(parsedFile.findAll('xml')))
+    # print(parsedFile)
+    isFirstPart = True
+    allRecords = []
+    if "xml" not in str(parsedFile):
+        print("NOT IN")
+    for xml in parsedFile.find_all(re.compile("xml")):
+        if (isFirstPart):
+            header = ["Submission Type", "Live Test Flag", "CIK", "ccc", "Period Of Report",
+                      "Report Calendar Or Quarter", "Is Amendment?", "Company Name", "Company Address: Street",
+                      "Company Address: City", "Company Address: State", "Company Address: Zipcode", "Report Type",
+                      "Form 13F file Number", "Provide Info for Instruction 5", "Name",
+                      "Title", "Phone", "Sigature", "City", "State of Country", "Signature Date", "Other included managers count",
+                      "Table Entry total", "Table Value Total", "Is Confidential Omitted"]
 
+            # allRecords.append(header)
+            for child in xml.find_all(True, recursive=False):
+                subR = []
+                for info in child.text.split('\n'):
+                    # subR = []
+                    if info != '':
+                        subR.append(info)
+                        # if isFirst:
+                        #     oneRecord += info + "\t\t\t\t"
+                        # else:
+                        #     oneRecord += info + "\t\t\t"
+                        #     isFirst = False
+                # print(subR)
+                # allRecords.append(subR)
+                # print(child.name + ":" +child.text)
+            for i in range (len(header)):
+                print(header[i] + ": " + subR[i])
+            print("\n$$$$$$$$$$$$$$$$$$$$$\n")
+            isFirstPart = False
+        else:
+            # print("NAME OF ISSUER" + "\t\t\t\t" + "TITLE OF CLASS" + "\t\t\t" + "CUSIP" + "\t\t\t" + "VALUE(X1000)" + "\t\t\t" + "SHARES/PRN AMOUNT" + "\t\t\t" + "SH/PRN" + "\t\t\t" + "INVESTMENT DISCRETION" + "\t\t\t" + "OTHER MANAGERS" + "\t\t\t" + "SOLE" + "\t\t\t" + "SHARED" + "\t\t\t" + "NONE" + "\t\t\t")
+            header = ["NAME OF ISSUER", "TITLE OF CLASS", "CUSIP", "VALUE(X1000)",
+                      "SHARES/PRN AMOUNT", "SH/PRN", "INVESTMENT DISCRETION", "OTHER MANAGERS", "SOLE", "SHARED", "NONE"]
+            allRecords.append(header)
+            for information in parsedFile.find_all(re.compile("infotable")):
+                # oneRecord = ""
+                # isFirst = True
+                # print(information.text.split('\n'))
+                subR = []
+                for info in information.text.split('\n'):
+                    # subR = []
+                    if info != '':
+                        subR.append(info)
+                        # if isFirst:
+                        #     oneRecord += info + "\t\t\t\t"
+                        # else:
+                        #     oneRecord += info + "\t\t\t"
+                        #     isFirst = False
+                # print(subR)
+                allRecords.append(subR)
+            # print(allRecords)
+            print(tabulate(allRecords))
+
+    print("\n\n**********************************************************")
+    print("************************* END OF *************************")
+    print("**********************************************************\n\n")
 
 def parseMethod2_Ready(parsedFile):
-    print("Partial ready")
+    print("***************** Partial ready ******************")
+    return
+
 
 # Main Program
 DocType = '13F'
@@ -72,7 +143,7 @@ documentsLink = findAllArchieveLink(parsedHTML) # find all 13F link
 
 # Note for no 13F found
 if len(documentsLink) == 0:
-    print("No 13F filing found for \n\tCIK/TICKER:" + str(CIK) + " \nYou can search another Ticker/CIK")
+    print("Warning: No 13F filing found for \n\tCIK/TICKER:" + str(CIK) + " \nYou can search another Ticker/CIK")
 
 for link in documentsLink:
     parsedHTML = inputURLgetParsedHTML(link)
